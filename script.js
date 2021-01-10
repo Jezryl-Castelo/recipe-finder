73ec6e2ec650e5f20fd9aa7648686f878f292086
Jezryl <yamabags@gmail.com>
2021-01-10 12:20:25 -0700

    link icon

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ const sushiImage = document.querySelector(".sushi");
 let searchQuery = '';
 const APP_ID = "0afafdc7";
 const APP_key = "69cb8af4aa436ea914b9305824fa8871";
+const input = document.querySelector('input')
+const searchIcon = document.querySelector('.search-icon');
 
+//Make form enter and icon click fire the submit event
+searchForm.addEventListener('submit', submitForm)
+searchIcon.addEventListener('click', submitForm)
 
-
-searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    searchQuery = e.target.querySelector('input').value;
+function submitForm() {
+    searchQuery = input.value;
     fetchAPI();
-})
+}
 
 async function fetchAPI() {
-    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}`;
+    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&to=48&app_id=${APP_ID}&app_key=${APP_key}`;
     const response = await fetch(baseURL);
     const data = await response.json();
     generateHTML(data.hits);
@@ -27,22 +30,32 @@ function generateHTML(results) {
     sushiImage.style.display = "none";
     let generatedHTML = '';
     results.map(result => {
+        let spaced = ""
+        result.recipe.healthLabels.map(ingredient => {
+            spaced.trimEnd(',');
+            spaced += ingredient + "," + " ";
+            
+        })
+        console.log(spaced)
         generatedHTML +=
-        `<div class="item">
-        <img src="${result.recipe.image}" alt="">
+        `
+        <div class="item">
+            <img src="${result.recipe.image}" alt="">
         <div class="flex-container">
             <h1 class="title">${result.recipe.label}</h1>
         </div>
-            
         <div class="details">
             <p class="item-data">
             <ion-icon name="flame-outline"></ion-icon>
             ${result.recipe.calories.toFixed(0)}</p>
-            <p class="restrictions">${result.recipe.healthLabels}</p>
-            
-            <a class="view-button" href="${result.recipe.url}">View Recipe</a>
+            <p class="restrictions">${spaced}</p>
+
+            <a class="view-button" target="_blank" href="${result.recipe.url}">View Recipe</a>
             </div>
         </div> `
     })
     searchResultDiv.innerHTML = generatedHTML;
 }
+
+
+
